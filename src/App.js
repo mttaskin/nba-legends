d1f147c9b4f9b9fecd9689224c1bfdbc140b8043
mttaskin <0mttaskin1@gmail.com>
2023-02-22 01:29:33 +0300

    nba-legends v4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Container className="text-center mt-4">
-      
+      <Header/>
     </Container>
   );
 }
